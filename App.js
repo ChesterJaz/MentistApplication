7d495738f59eb6da7ad65b05e27fba306ef2d123
Chester <89991461+ChesterJaz@users.noreply.github.com>
2023-08-24 21:08:01 +0800

    update

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import {View, Text} from 'react-native';
+import {Text, SafeAreaView} from 'react-native';
 import React from 'react';
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <SafeAreaView className="bg-red-200">
+      <Text className="text-2xl text-black dark:text-white">App</Text>
+    </SafeAreaView>
   );
 }
